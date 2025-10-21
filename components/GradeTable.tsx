6e80cab7bbd519b4e5782212fcabd9b41358d0fa
LouisChabanon <louis@chabanon.es>
2025-10-21 20:51:18 +0200

    added proper loading state to grade table

--- components/GradeTable.tsx
+++ components/GradeTable.tsx
@@ -85,6 +85,7 @@ export function GradeTable(){
             placeholder="Rechercher..."
             value={searchTerm}
             onChange={e => setSearchTerm(e.target.value)}
+            disabled={isLoading}
             className="px-4 py-2 border border-buttonSecondaryBorder bg-backgroundSecondary rounded-md w-full sm:w-1/2 focus:outline-none focus-ring-2"
           />
           <div className="flex gap-2">
@@ -105,7 +106,15 @@ export function GradeTable(){
           </div>
         </div>
       {isLoading ? (
-          <div className="text-center text-textTertiary">Chargement...</div>
+          <div className="text-center text-textTertiary py-8 bg-backgroundPrimary rounded-lg w-full h-full animate-pulse flex flex-col items-center justify-center">
+            <div>
+              <svg className="mr-3 size-5 animate-spin inline-block" viewBox="0 0 24 24">
+                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+              </svg>
+            En attente de Lise ... (c'est long)
+            </div>
+            </div>
         ) : (
           <>
             <div className="overflow-auto h-full rounded-lg shadow-md">
