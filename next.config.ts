3b03c19439bc0265de23937c45ba7ec4d92731ba
LouisChabanon <louis@chabanon.es>
2025-11-01 20:28:32 +0100

    attempt at fixing app icon on IOS

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,4 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true
-});
-
-module.exports = withPWA({
+const nextConfig = {
   reactStrictMode: true,
-});
-
-module.exports = {
   output: 'standalone'
-};
+}
