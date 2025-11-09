80d1d3d3c8b935347683ce15bded9f4b56fa3ff9
LouisChabanon <louis@chabanon.es>
2025-11-09 10:25:37 +0100

    Fixed absence table fetching Removed database call, absence are not stored for now

--- actions/GetAbsences.ts
+++ actions/GetAbsences.ts
@@ -16,15 +16,16 @@ export async function getAbsenceData(reload: boolean = true): Promise<AbsencesRe
 
     const session = (await verifySession());
     if(!session.username){
-        console.error("No username found in session.")
+        logger.warn("User has no active session, failed to fetch absences")
         return {errors: "No username found in session.", success: false};
     }
-    const user = await prisma.user.findUnique({ where: { username: session.username} })
 
-    if (!user) {
-        console.error("User not found in database.");
-        return {errors: "User not found in database.", success: false};
-    }
+    // const user = await prisma.user.findUnique({ where: { username: session.username} })
+
+    // if (!user) {
+    //     console.warn("User not found in database.");
+    //     return {errors: "User not found in database.", success: false};
+    // }
 
     const jsessionid = session.sessionId
 
@@ -38,7 +39,7 @@ export async function getAbsenceData(reload: boolean = true): Promise<AbsencesRe
     //if (!reload) absences.push(db_absences)
 
     if(reload === true){
-        console.log("Fetching absences from Lise")
+        logger.info("Fetching absences from Lise", {username: session.username})
 
         const jar = new CookieJar();
         const controller = new AbortController();
@@ -55,7 +56,7 @@ export async function getAbsenceData(reload: boolean = true): Promise<AbsencesRe
             
             
             if ($html('title').text().includes('Connectez-vous') || $html('title').text().includes('Sign in')){
-                console.error("User not connected or session expired")
+                logger.error("User session has expired on LISE", {username: session.username})
                 await deleteSession();
                 return {errors: "Session has expired", success: false};
             }
@@ -188,10 +189,12 @@ export async function getAbsenceData(reload: boolean = true): Promise<AbsencesRe
                 const $table_html = cheerio.load(html_third_req);
                 const nbrTotalAbs = $table_html('#form\\:nbrAbs').text() != "" ? parseInt($table_html('#form\\:nbrAbs').text()) : 0
                 const dureeTotalAbs = $table_html('#form\\:dureeAbs').text() != "" ? $table_html('#form\\:dureeAbs').text() : "00h00"
-                
-                const rows = $table_html('#form\\table_data > tr');
+
+                const rows = $table_html('#form\\:table_data');
                 rows.each((index, element) => {
                     const cells = $table_html(element).find('td');
+
+                    // If no absences, date is set to "Aucune absence." Fine for now but it's not pretty
                     const rowData: AbsenceType = {
                         date: $table_html(cells.eq(0)).clone().text().trim(),
                         motif: $table_html(cells.eq(1)).clone().text().trim(),
@@ -203,16 +206,17 @@ export async function getAbsenceData(reload: boolean = true): Promise<AbsencesRe
                     }
 
                     absences.push(rowData);
-
                 })
 
             
-            logger.info("Sucessfully fetched absences", {user: user.username, nbrTotalAbs, dureeTotalAbs});
-            console.log(absences)
+            logger.info("Sucessfully fetched absences", {user: session.username, nbrTotalAbs, dureeTotalAbs});
             return {success: true, data: {nbTotalAbsences: nbrTotalAbs, dureeTotaleAbsences: dureeTotalAbs, absences}}
             
         }catch(error){
-            console.error("Error fetching absences: ", error)
+            logger.error("Error fetching absences: ", {
+                error: error instanceof Error ? error.message : String(error),
+                stack: error instanceof Error ? error.stack : undefined
+            })
             return {errors: "Error fetch absences", success: false}
         }
     }
